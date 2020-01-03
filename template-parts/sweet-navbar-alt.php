232fecf1d6e839af617099dfc383a06c9aed15da
martinzorrilla <martinzorrill@gmail.com>
2020-01-03 18:03:27 -0300

    changed the top nav bar

--- template-parts/sweet-navbar-alt.php
+++ template-parts/sweet-navbar-alt.php
@@ -1,11 +1,16 @@
-<div class="row expanded app-dashboard-top-nav-bar">
-    <div class="columns large-6 medium-2">
-      <button data-toggle="app-dashboard-sidebar" class="menu-icon hide-for-medium"></button>
-      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="app-dashboard-logo">SweetDoc</a>
-    </div>
-    
-    <div class="columns large-6 shrink app-dashboard-top-bar-actions">
-      <button href="#" class="button hollow">Salir</button>
-      <a href="#" height="30" width="30" alt=""><i class="fa fa-info-circle"></i></a>
-    </div>
+<!-- This demo uses float grid but you can use flex grid too -->
+
+<div class="top-bar" id="example-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text">SweetDoc!</li>
+    </ul>
   </div>
+  <div class="top-bar-right">
+  <ul class="dropdown menu" data-dropdown-menu>
+      <li><a href="#0">One</a></li>
+      <li><a href="#0">Two</a></li>
+      <li><a href="#0">Three</a></li>
+    </ul>
+  </div>
+</div>
