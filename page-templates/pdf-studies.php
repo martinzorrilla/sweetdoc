65fe1d802ad235e9dad77905ef9039c2eb34b606
martinzorrilla <martinzorrill@gmail.com>
2020-04-15 12:40:37 -0400

    minor changes insd studies pdf

--- page-templates/pdf-studies.php
+++ page-templates/pdf-studies.php
@@ -8,28 +8,53 @@ function Header()
 {
     // Cabacera
     global $title;
-    //$header_title = 'Dra. Diana Andrea Zorrilla';
+    global $y_actual;
+    global $y_new;
+    //global $page_height;
+
 
     $this->SetFont('Arial','B',15);
+    $this->Ln(30);
     $w = $this->GetStringWidth($title)+6;
     $this->SetX((210-$w)/2);
     $this->SetDrawColor(157,165,170); 
     $this->SetFillColor(255, 255, 255);
     $this->SetTextColor(0,0,0);
     $this->SetLineWidth(1);
-    $this->Cell($w,9,$title,1,1,'C',true);
-    $this->Ln(10);
+    //$this->Cell($w,9,$title,1,1,'C',true);
+    $this->Ln(20);
     // Guardar ordenada
     $this->y0 = $this->GetY();
 }
 
+// function Footer()
+// {
+//     // Pie de página
+//     $this->SetY(-15);
+//     $this->SetFont('Arial','I',8);
+//     $this->SetTextColor(128);
+//     $this->Cell(0,10,'Dra. Andrea Zorrilla - Pagina '.$this->PageNo(),0,0,'C');
+// }
+
 function Footer()
 {
     // Pie de página
-    $this->SetY(-15);
-    $this->SetFont('Arial','I',8);
-    $this->SetTextColor(128);
-    $this->Cell(0,10,'Pagina '.$this->PageNo(),0,0,'C');
+    $this->SetY(-45);
+//    $this->SetFont('Arial','I',8);
+    $this->SetFont('Times','',12);
+
+    //$this->SetTextColor(128);
+    $this->SetX(110);
+    $this->Cell(0,12,'...............................................',0,2);
+    $this->SetX(118);
+    $this->Cell(0,5,'Dra. Andrea Zorrilla',0,2);
+    $this->SetX(114);
+    $this->Cell(0,5,utf8_decode('Ginecología y Obstetricia'),0,2);
+    $this->SetX(106);
+    $this->Cell(0,5,utf8_decode('Especialista en TGI y colposcopia'),0,2);
+    $this->SetX(125);
+    $this->Cell(0,5,utf8_decode('RP: 11.220'),0,2);
+    $this->SetX(0);
 }
 
 function SetCol($col)
@@ -69,7 +94,7 @@ function ChapterTitle($num, $label)
     $this->SetFont('Arial','',12);
     $this->SetFillColor(200,220,255);
     //$this->Cell(0,6,"PARTE $num : $label",0,1,'L',true);
-    $this->Cell(0,6,"$label",0,1,'L',true);
+    $this->Cell(0,6,"$label",0,1,'C',true);
     $this->Ln(4);
     // Guardar ordenada
     $this->y0 = $this->GetY();
@@ -83,14 +108,55 @@ function ChapterBody($file)
     
     // Fuente
     $this->SetFont('Times','',12);
-    // Imprimir texto en una columna de 6 cm de ancho
-    $this->MultiCell(80,5,$txt);
+    // Imprimir texto en una columna de 6 cm de ancho (si el valor es 60)
+    $this->MultiCell(190,7,$txt);
+    //$this->Cell(0,5,$txt);
+    
     $this->Ln();
     // Cita en itálica
     $this->SetFont('','I');
-    $this->Cell(0,5,'(fin de la prescripcion)');
+    $this->Cell(0,5,'(fin de la seccion)');
     // ir a la segunda columna
-    $this->SetCol(1);
+    //$this->SetCol(1);
+    
+    $this->Ln(14);
+    // Guardar ordenada
+    $this->y0 = $this->GetY();
+}
+
+function ImageBody($num, $eje_y, $file)
+{
+    
+    //$this->SetX(20);
+    $abscissa = 50; // eje X
+    $ordenada = $eje_y; // eje Y
+    $image_height = 30;
+    
+    $abscissa_inicial = $abscissa; // eje X
+    $ordenada_inicial = $ordenada; // eje Y
+    
+    switch ($num) {
+        case 1:
+            $abscissa = $abscissa_inicial + 60;
+            $ordenada = $ordenada_inicial;
+            break;
+        case 2:
+            $abscissa = $abscissa_inicial;
+            $ordenada = $ordenada_inicial + $image_height;
+            break;
+        case 3:
+            $abscissa = $abscissa_inicial + 60;
+            $ordenada = $ordenada_inicial + $image_height;
+            break;
+        case 4:
+            $abscissa = $abscissa_inicial;
+            $ordenada = $ordenada_inicial + 2*$image_height + 10;
+            break;
+    }
+
+    $this->Image($file,$abscissa,$ordenada,50,30,'JPG');
+    //$this->Cell(50,10,'texto de prueba'.$num.' -> Y='.$this->GetY(), 1);
+    $this->Ln();
 }
 
 function PrintChapter($num, $title, $file)
@@ -101,27 +167,167 @@ function PrintChapter($num, $title, $file)
     //$this->ChapterBody($file);
 }
 
-function PrintPrescription($num, $title, $file)
+function PrintSection($num, $title, $file)
 {
     // Añadir capítulo
     //$this->AddPage();
-    //$this->ChapterTitle($num,$title);
-    $this->ChapterBody($file);
+    $this->ChapterTitle($num,$title);
+    //$this->ChapterBody($file);
 }
+
+function PrintElement($num, $title, $file)
+{
+    if (!empty($file)) {
+
+        $txt = $title.": ".$file;
+        // Fuente
+        $this->SetFont('Times','',12);
+        // Imprimir texto en una columna de 6 cm de ancho (si el valor es 60)
+        $this->MultiCell(190,7,$txt);
+        //$this->Cell(0,5,$txt);
+        //$this->Ln();
+        // Guardar ordenada
+        $this->y0 = $this->GetY();
+    }
+}
+
+function PrintSecondaryTitle($num, $title, $file)
+{
+        
+        // Fuente
+        $this->SetFont('Times','B',12);
+        // Imprimir texto en una columna de 6 cm de ancho (si el valor es 60)
+        $this->MultiCell(190,7,$title);
+        //$this->Cell(0,5,$txt);
+        //$this->Ln();
+        // Guardar ordenada
+        $this->y0 = $this->GetY();
+}
+
+function PrintArray($num, $title, $array)
+{
+
+    // si es array y todos sus elementos no son vacios
+    if (is_array($array) && array_filter($array)){
+        //primero eliminar los elementos vacios
+        $emptyRemoved = array_filter($array);
+        //unir todos los elementos del array en un string
+        $arrayToString = implode( ", ", $emptyRemoved );
+        //como lo que devuelve son los propios valores de la BD y no los labels, remover los "_"
+        $arrayToString = str_replace("_", " ", $arrayToString);
+        //$pdf->PrintElement(2,$title,$arrayToString);
+        
+        $txt = $title.": ".$arrayToString;
+        // Fuente
+        $this->SetFont('Times','',12);
+        // Imprimir texto en una columna de 6 cm de ancho (si el valor es 60)
+        $this->MultiCell(190,7,$txt);
+        //$this->Cell(0,5,$txt);
+        //$this->Ln();
+        // Guardar ordenada
+        $this->y0 = $this->GetY();
+    }
+}
+
+function PrintEvaluacionGeneral($num, $radiobox_evaluacion_general, $checkbox_motivo_inadecuada)
+{
+
+    $arrayToString = "";
+    // si es array y todos sus elementos no son vacios
+    if (is_array($checkbox_motivo_inadecuada) && array_filter($checkbox_motivo_inadecuada)){
+        //primero eliminar los elementos vacios
+        $emptyRemoved = array_filter($checkbox_motivo_inadecuada);
+        //unir todos los elementos del array en un string
+        $arrayToString = implode( ", ", $emptyRemoved );
+        //como lo que devuelve son los propios valores de la BD y no los labels, remover los "_"
+        $arrayToString = str_replace("_", " ", $arrayToString);
+        $arrayToString = " por ".$arrayToString;
+        
+        //$pdf->PrintElement(2,$title,$arrayToString);
+    }
+    
+    if (!empty($radiobox_evaluacion_general)) {
+        $txt = " - Evaluacion general: ".$radiobox_evaluacion_general.$arrayToString;;
+        $this->SetFont('Times','',12);
+        $this->MultiCell(190,7,$txt);
+        $this->y0 = $this->GetY();
+    }
+}
+
+
+function PrintImage($num, $eje_y, $file)
+{
+    $this->ImageBody($num, $eje_y, $file);    
+}
+
+function CheckPageSpaceLeft($page_height, $current_y)
+{
+    //$this->GetPageHeight();
+    $espacio_min_inferior = 115;
+    //$espacio_min_inferior = 95;
+    $space_left = $page_height - $current_y;
+    if ($space_left < $espacio_min_inferior) {
+        $this->AddPage();
+    }
+}
+
 }//class
 
+
+// pure php data ----------------------------------------------
 $studies_id = $_GET['studies_id'];
 $studies_fields = get_post_custom($studies_id);
-  
-$egcv = $studies_fields['egcv'][0];
-$egcv_dx = $studies_fields['egcv_dx'][0];
 
+$colpo_data_post = get_post_custom($colpo_post_id);
+$patient_id = $studies_fields['related_study'][0];
+
+$patient_fields = get_post_custom($patient_id);
+$name = $patient_fields['nombre'][0];
+$lastname = $patient_fields['apellido'][0];
+$cedula = $patient_fields['cedula'][0];
+$fullname = $name.' '.$lastname;
+$fecha_de_nacimiento = $patient_fields['fecha_de_nacimiento'][0] !="" && $patient_fields['fecha_de_nacimiento'][0] !=NULL ? $patient_fields['fecha_de_nacimiento'][0] : "";
+//$bday = new DateTime('23.8.1988'); // Your date of birth
+$bday = new Datetime(date('d.m.y'));
+if ($fecha_de_nacimiento != ""){ $bday = new Datetime(date('d.m.y', strtotime($fecha_de_nacimiento)));}
+$today = new Datetime(date('d.m.y'));
+$diff = $today->diff($bday);
+$edad_paciente = $diff->y;
+$creation_date = get_the_date( 'd-m-Y', $colpo_post_id ); //fecha de creacion de colpo puede no ser == a fecha de la consulta debido a que se puede crear una consulta sin colpo y luego editar
+$datos_personales = $fullname."        Edad: ".$edad_paciente."        Ci: ".$cedula."        Fecha: ".$creation_date;
+//$datos_personales1 = $fullname."                        Edad: ".$edad_paciente;
+//$datos_personales2 = $cedula."                        Fecha de consulta: ".$creation_date;
+
+
+
+$checkbox_egcv = get_field('egcv', $studies_id);
+$egcv_dx = $studies_fields['egcv_dx'][0];
+wp_die(var_dump($patient_id));
+
+
+// fpdf --------------------------------------------
 $pdf = new PDF();
-$title = 'Dra. Diana Andrea Zorrilla';
-$pdf->SetTitle($title);
-$pdf->SetAuthor('Andrea Zorrilla');
-$pdf->PrintChapter(1,' EGCV.                                                                   -            egcv_dx', $myfile);
-$pdf->PrintPrescription(1,'egcv', $egcv);
-$pdf->PrintPrescription(2,'egcv_dx',$egcv_dx);
+//$pdf->SetAutoPageBreak(true, 100);
+$pdf->SetAutoPageBreak(true, 0);
+$pdf->SetAuthor('Dra. Andrea Zorrilla');
+$title = 'Solicitud de estudios';
+//$title = $fullname;
+//$pdf->SetTitle($title);
+
+$pdf->AddPage();
+$page_height = $pdf->GetPageHeight();
+$pdf->PrintSection(1,'DATOS PERSONALES', $fullname);
+$pdf->PrintElement(2,utf8_decode(' - Nombre'),$datos_personales);
+
+$pdf->Ln(4);
+$pdf->PrintSection(2,utf8_decode('ESTUDIOS SOLICITADOS'), $fullname);
+$pdf->PrintArray(2,utf8_decode(' - Ecografía ginecológica transvaginal'),$checkbox_egcv);
+$pdf->PrintElement(2,' - DESCRIPCION',$egcv_dx);
+// $pdf->PrintSecondaryTitle(2,utf8_decode(' - Hallazgos colposcopicos anormales'), "");
+
+//El autoPagaBreak esta desactivado y lo hago manualmente para la seccion de imagenes. esi implica que si el texto de la seccion hallazgos es muy larga no hara el salto de pagian automaticamente
+//$pdf->AddPage();
+
+
 $pdf->Output();
 ?>
